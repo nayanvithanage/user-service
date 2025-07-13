14b208799ce49d6cb29f79f3e30b3c39cc63edd2
Nayan VIthanage <nayan.vithanage@gmail.com>
2025-07-13 11:58:27 +0530

    Add GitHub Actions CI for user service

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
-// db.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('exploresl', 'postgres', '1212', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'exploresl',
+  process.env.DB_USER || 'postgres',
+  process.env.DB_PASS || '1212',
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+  }
+);
 
 module.exports = sequelize;
